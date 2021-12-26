8b324c81ceab706ac8d0727d73cba502663b4302
awedvhu <s1082858@gm.pu.edu.tw>
2021-12-27 01:25:49 +0800

    user

--- myapp/admin.py
+++ myapp/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from myapp.models import user
 
 # Register your models here.
+class userAdmin(admin.ModelAdmin):
+    list_display=('name', 'JID', 'gentle', 'phone', 'gmail', 'password')
+admin.site.register(user, userAdmin)
--- myapp/models.py
+++ myapp/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+class user(models.Model):
+    JID = models.CharField(max_length=15, primary_key=True, unique=True)
+    name = models.CharField(max_length=15)
+    gentle = models.CharField(max_length=2)
+    phone = models.CharField(max_length=15, unique=True)
+    gmail = models.EmailField(max_length=50, unique=True)
+    password = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.name
